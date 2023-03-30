e4c16ba040e50f441d74138bc78ffcb8b891cb95
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:13:58 +0300

    Corrected typo

--- app/serializers/power_serializer.rb
+++ app/serializers/power_serializer.rb
@@ -1,11 +1,11 @@
 class PowerSerializer < ActiveModel::Serializer
 
-  attributes :id,:name ,:description
+  attributes :id, :name ,:description
   
   def include_heros?
     @instance_options[:include_heros]
   end
 
   has_many :heros, if: :include_heros?
-  
+
 end
