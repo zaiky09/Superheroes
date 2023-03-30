df53086bb00f8a5b9a06fcdece765cb6ef0bcc20
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 15:58:05 +0300

    Added validations to HeroPower model

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -3,6 +3,10 @@ class HeroPower < ApplicationRecord
     belongs_to :power
     belongs_to :hero
 
-    
+    STRENGTH_OPTIONS = %w[Strong Weak Average].freeze
+
+    validates :strength,{
+    inclusion: { in: STRENGTH_OPTIONS, case_sensitive: false ,message: "must be either Strong, Weak, or Average"}
+    }
 
 end
