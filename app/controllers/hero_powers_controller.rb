01879a830a94914586411301c03501347b700577
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:09:03 +0300

    Setup HeroPowersController

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,20 @@
 class HeroPowersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create 
+        heropow = HeroPower.create!(heropow_params)
+        render json: heropow.hero, include_powers: true, status: :created
+    end
+
+
+    private 
+    def heropow_params 
+        params.permit(:strength, :hero_id, :power_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
