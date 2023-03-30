1754596101a8f6f03760096881fd3a1e1cc5a1f4
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:16:20 +0300

    Setup Hero serializer

--- app/serializers/hero_serializer.rb
+++ app/serializers/hero_serializer.rb
@@ -1,3 +1,11 @@
 class HeroSerializer < ActiveModel::Serializer
-  attributes :id
+
+  attributes :id, :name , :super_name
+
+  def include_powers?
+    @instance_options[:include_powers]
+  end
+
+  has_many :powers, if: :include_powers?
+
 end
