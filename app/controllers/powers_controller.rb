2842495db4374b85881c60645db0629142b5bff0
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:05:35 +0300

    Setup PowersController

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,42 @@
 class PowersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    # GET /powers
+    def index 
+        powers = Power.all 
+        render json: powers
+    end
+
+    # GET powers/:id
+    def show 
+        power = find_power
+        render json: power, include_heros: true
+    end
+
+    # PUT /powers/:id
+    def update 
+        power = find_power
+        power.update!(power_params)
+        render json: power, status: :accepted
+    end
+
+    private 
+    def render_not_found_response 
+        render json: {error: "Power not found"}, status: :not_found
+    end
+
+    def find_power
+        Power.find(params[:id])
+    end
+
+    def power_params 
+        params.permit(:description)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
