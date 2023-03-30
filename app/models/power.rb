0b503ceedb112917a4452d1067001d23681cb938
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 15:53:43 +0300

    Added has_many relation to Power model

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,3 +1,10 @@
 class Power < ApplicationRecord
+
+    has_many :hero_powers
+    has_many :heros, through: :hero_powers
+
+    validates :description, {
+        length: {minimum: 20}
+    }
     
 end
