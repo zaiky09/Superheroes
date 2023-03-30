b09a4130035fa813f1bd6cd9afe272e27d2ac3c7
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:02:40 +0300

    Setup HeroesController

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,25 @@
 class HeroesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /heroes
+    def index 
+        heroes = Hero.all 
+        render json: heroes
+    end
+
+    # GET /heroes/:id
+    def show 
+        hero = find_hero
+        render json: hero, include_powers: true
+    end
+
+    private 
+    def render_not_found_response 
+        render json: {error: "Hero not found"}, status: :not_found
+    end
+
+    def find_hero
+        Hero.find(params[:id])
+    end
 end
