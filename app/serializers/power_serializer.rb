0ed1dc58a881863885fa3ce22324fb127a21d1e0
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:13:26 +0300

    Setup Power serializer

--- app/serializers/power_serializer.rb
+++ app/serializers/power_serializer.rb
@@ -1,3 +1,11 @@
 class PowerSerializer < ActiveModel::Serializer
-  attributes :id
+
+  attributes :id,:name ,:description
+  
+  def include_heros?
+    @instance_options[:include_heros]
+  end
+
+  has_many :heros, if: :include_heros?
+  
 end
