1d5ef23483c1a864a89bcf7171e92b7bf00e26a1
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 16:38:47 +0300

    Added seed data and secret.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :hero_powers, only: [:create]
+  resources :powers, only: [:index,:show,:update]
+  
+
+  get '/heroes', to:"heroes#index"
+
+  get '/heroes/:id', to:"heroes#show"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Avengers Assembling 🦸"
+
+hero = Hero.create([  
+
+    {  "name": "Zamil Mopower", "super_name": "Mr. Right" },  
+    
+    {  "name": "Faith Green", "super_name": "Prayer Girl" },  
+    
+    {  "name": "Ali Mona", "super_name": "In debt" }
+    
+    ])
+
+power = Power.create([
+    {
+      "name": "super speed",
+      "description": "faster than a speeding bullet"
+    },
+    {
+      "name": "Teleport",
+      "description": "The ultimate jumpman"
+    }
+  ])    
+ 
+hero-power = HeroPower.create([{
+    "strength": "Strong",
+    "power_id": 3,
+    "hero_id": 1
+  },
+  {
+    "strength": "Weak",
+    "power_id": 1,
+    "hero_id": 3
+  },
+  {
+    "strength": "Average",
+    "power_id": 2,
+    "hero_id": 2
+  }])  
+
+  puts "Mission complete"
